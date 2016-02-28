b35cba330e8ee8caef1f207ec83c0bdabed0b563
Cloud Project <lps117@naver.com>
2016-02-28 10:00:16 +0900

    Hello, World !

---
+++ S01/S01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  S01
+//
+//  Created by 구름 on 2016. 2. 28..
+//  Copyright © 2016년 Cloud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
